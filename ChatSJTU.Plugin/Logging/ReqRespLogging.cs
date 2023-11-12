9b156e9727854a45692e4143b823007c17d53de8
Mike Smith <1357310795@qq.com>
2023-11-12 20:30:58 +0800

    add logging

---
+++ ChatSJTU.Plugin/Logging/ReqRespLogging.cs
@@ -0,0 +1,36 @@
+﻿using ChatSJTU.Plugin.Weather;
+
+namespace ChatSJTU.Plugin.Log
+{
+    public class RequestResponseLoggingMiddleware : IMiddleware
+    {
+        private readonly ILogger<RequestResponseLoggingMiddleware> _logger;
+
+        public RequestResponseLoggingMiddleware(ILogger<RequestResponseLoggingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            HttpRequest request = context.Request;
+            var url = request.Path.ToString();
+            var method = request.Method;
+            var excuteStartTime = DateTime.Now;
+
+            //获取Response.Body内容
+            var respStatus = context.Response.StatusCode;
+
+            _logger.LogInformation($"[{excuteStartTime.ToString("g")}][{respStatus}] {method} {url}");
+            return Task.CompletedTask;
+        }
+    }
+
+    public static class RequestResponseLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestResponseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+        }
+    }
+}
