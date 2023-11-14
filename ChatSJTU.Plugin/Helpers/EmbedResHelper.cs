ad0052ad173179da24944cc5c4d65b8428cc026b
1357310795 <1357310795@qq.com>
2023-11-14 09:51:16 +0800

    use svg icons

---
+++ ChatSJTU.Plugin/Helpers/EmbedResHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSJTU.Plugin.Helpers
+{
+    public partial class EmbedResHelper
+    {
+        [EmbedResourceCSharp.FileEmbed("Resources/交我算.svg")]
+        public static partial ReadOnlySpan<byte> Get交我算();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/生活.svg")]
+        public static partial ReadOnlySpan<byte> Get生活();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/校园管理.svg")]
+        public static partial ReadOnlySpan<byte> Get校园管理();
+    }
+}
