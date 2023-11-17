ed85603acf24f25d79bafcac4a7c2cc2844f4edb
1357310795 <1357310795@qq.com>
2023-11-17 19:43:48 +0800

    add

--- ChatSJTU.Plugin/Helpers/EmbedResHelper.cs
+++ ChatSJTU.Plugin/Helpers/EmbedResHelper.cs
@@ -16,5 +16,23 @@ namespace ChatSJTU.Plugin.Helpers
 
         [EmbedResourceCSharp.FileEmbed("Resources/校园管理.svg")]
         public static partial ReadOnlySpan<byte> Get校园管理();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/财务.svg")]
+        public static partial ReadOnlySpan<byte> Get财务();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/场馆预约.svg")]
+        public static partial ReadOnlySpan<byte> Get场馆预约();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/教务.svg")]
+        public static partial ReadOnlySpan<byte> Get教务();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/教学楼.svg")]
+        public static partial ReadOnlySpan<byte> Get教学楼();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/日程.svg")]
+        public static partial ReadOnlySpan<byte> Get日程();
+
+        [EmbedResourceCSharp.FileEmbed("Resources/图书馆.svg")]
+        public static partial ReadOnlySpan<byte> Get图书馆();
     }
 }
--- ChatSJTU.Plugin/Services/FunctionDef.cs
+++ ChatSJTU.Plugin/Services/FunctionDef.cs
@@ -30,7 +30,7 @@ namespace ChatSJTU.Plugin.Services
                         {
                             "id": "campus",
                             "name": "SJTU 校园生活",
-                            "description": "为模型提供获取 SJTU 校园生活数据的能力，如天气、食堂热力图、图书馆上座率等",
+                            "description": "为模型提供获取 SJTU 校园生活数据的能力，如天气、食堂热力图等",
                             "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get交大生活())}}",
                             "functions": [
                                 {{WeatherController.GetDef()}},
@@ -51,10 +51,58 @@ namespace ChatSJTU.Plugin.Services
                         {
                             "id": "hpc",
                             "name": "SJTU 超算平台",
-                            "description": "为模型提供获取超算平台使用帮助、实时利用率的能力",
+                            "description": "为模型提供获取“交我算”校级计算平台使用帮助、实时利用率的能力",
                             "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get交我算())}}",
                             "functions": [
                                 {{HpcOverviewController.GetDef()}}
+                            ]
+                        },
+                        {
+                            "id": "building",
+                            "name": "SJTU 教学楼",
+                            "description": "为模型提供获取教学楼信息的能力，如教学楼地图、教室排课情况、教室实时状态等",
+                            "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get教学楼())}}",
+                            "functions": [
+                            ]
+                        },
+                        {
+                            "id": "library",
+                            "name": "SJTU 图书馆",
+                            "description": "为模型提供获取图书馆信息的能力，如在馆人数、书籍查找、借用情况查询等",
+                            "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get图书馆())}}",
+                            "functions": [
+                            ]
+                        },
+                        {
+                            "id": "calendar",
+                            "name": "我的日程",
+                            "description": "为模型提供添加日程、查询日程的能力",
+                            "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get日程())}}",
+                            "functions": [
+                            ]
+                        },
+                        {
+                            "id": "jiaowu",
+                            "name": "教务信息",
+                            "description": "为模型提供课程信息、成绩查询的能力",
+                            "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get教务())}}",
+                            "functions": [
+                            ]
+                        },
+                        {
+                            "id": "caiwu",
+                            "name": "财务信息",
+                            "description": "为模型提供工资查询、学费查询的能力",
+                            "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get财务())}}",
+                            "functions": [
+                            ]
+                        },
+                        {
+                            "id": "space",
+                            "name": "场馆预约",
+                            "description": "为模型提供各类场馆预约的能力，包括小组自习室、面试空间、空教室等",
+                            "icon": "data:image/svg+xml;base64,{{Convert.ToBase64String(EmbedResHelper.Get场馆预约())}}",
+                            "functions": [
                             ]
                         }
                     ]
