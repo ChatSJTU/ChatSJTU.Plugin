7b00f0e0ba4a68627e4bc1fc4844a46ed2587e3d
Mike Smith <1357310795@qq.com>
2024-09-09 16:20:00 +0800

    delete unuseful token

--- ChatSJTU.Plugin/Program.cs
+++ ChatSJTU.Plugin/Program.cs
@@ -1,8 +1,3 @@
-
-using Quartz.Impl;
-using Quartz;
-using static Quartz.Logging.OperationName;
-using ChatSJTU.Plugin.Services.Jaccount;
 using ChatSJTU.Plugin.Services;
 using ChatSJTU.Plugin.Log;
 
@@ -13,26 +8,7 @@ namespace ChatSJTU.Plugin
         public static void Main(string[] args)
         {
             GlobalCookie.CookieContainer = new System.Net.CookieContainer();
-
-            StdSchedulerFactory factory = new StdSchedulerFactory();
-            //创建任务调度器
-            IScheduler scheduler = factory.GetScheduler().GetAwaiter().GetResult();
-            //启动任务调度器
-            scheduler.Start();
-
-            IJobDetail job = JobBuilder.Create<JacTokenRefreshJob>()
-                .WithIdentity("JacTokenRefreshJob", "Test")
-                .Build();
-            ITrigger trigger = TriggerBuilder.Create()
-               .WithIdentity("JacTokenRefreshJobTrigger", "Test")
-               .WithSimpleSchedule(x =>
-               {
-                   x.WithIntervalInMinutes(59).RepeatForever();
-               })
-               .StartNow()
-               .Build();
-            //将创建的任务和触发器条件添加到创建的任务调度器当中
-            scheduler.ScheduleJob(job, trigger);
+            GlobalCookie.Read();
 
             var builder = WebApplication.CreateBuilder(args);
 
